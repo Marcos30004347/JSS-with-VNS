29eef5e96650e3f15abdeb0c2e4a37df25f276f4
maTh51 <matheustigon@gmail.com>
2021-03-17 20:53:07 -0300

    adding gap

--- benchmark.py
+++ benchmark.py
@@ -13,5 +13,12 @@ subprocess.call(cmd)
 for case in data["instances"]:
     subprocess.call(["./jss", case["path"], "15", "15", "15"])
 
+r = open("out.txt")
+r.readline()
+
+for otimo in data["instances"]:
+    bt = float(r.readline())
+    gap = float(bt - float(otimo["optimum"]))/float(otimo["optimum"])
+    print("{}:\notimo: {} | obtido: {} | gap {}".format(otimo["name"], otimo["optimum"], bt, gap))
 
 f.close()
